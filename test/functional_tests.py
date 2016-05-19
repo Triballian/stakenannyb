5ce94a1c72403a58ffb78f145bc8cdef50aa5b22
Triballian <candiapps@gmail.com>
2016-05-19 14:11:29 -0500

    Revert "removed test project it wasnt working"
    This reverts commit ee0e8357d1e39ced4070997909705402b32292bd.

--- test/functional_tests.py
+++ test/functional_tests.py
@@ -14,10 +14,9 @@ import unittest
 from sys import path as spath
 
 #import stakenannyb as sn
-#chdir('..\stakenannyb')
+
 spath.append('..\stakenannyb')
 import stakenannyb as sn
-from stakenannyb.filemakeifno import filemakeifno
 
 
  
@@ -28,8 +27,8 @@ class initialSessionTest(unittest.TestCase):
     #def tearDown(self):
     #    self.sn.commandquit()
 
-    #def test_setup_is_correct(self):
-    #    self.assertTrue(sn.setup())
+    def test_setup_is_correct(self):
+        self.assertTrue(sn.setup())
 
     def test_issncmdfileinstantupifnodo_is_correct(self):
         self.assertTrue(sn.setup.issncmdfileinstartupifnodo())
@@ -53,7 +52,7 @@ class initialSessionTest(unittest.TestCase):
  
     def test_can_verify_session_integrety_and_prompt_for_options_based_on_findings(self):
         # PID 1 is there a datafolder, if not make one
-        self.assertTrue(finddirmakeifno())
+        self.assertTrue(sn.appdirmakeifno())
 
         #don't forget to test the app reaction to a session alreadly running
 
